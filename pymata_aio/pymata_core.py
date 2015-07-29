4ac54d8869383b302a88637a474c457636d52349
Alan Yorinks <MisterYsLab@gmail.com>
2015-07-29 18:14:15 -0400

    Update pymata_core.py
    Added correction for autodetect of OS X ports.

--- pymata_aio/pymata_core.py
+++ pymata_aio/pymata_core.py
@@ -21,6 +21,7 @@ import sys
 import time
 import signal
 import logging
+import glob
 
 import serial
 
@@ -1141,13 +1142,21 @@ class PymataCore:
         This method attempts to discover the com port that the arduino is connected to.
         @return: Detected Comport
         """
-        locations = ['dev/ttyACM0', '/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyACM2', '/dev/ttyACM3', '/dev/ttyACM4',
-                     '/dev/ttyACM5', '/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2', '/dev/ttyUSB3', '/dev/ttyUSB4',
-                     '/dev/ttyUSB5', '/dev/ttyUSB6', '/dev/ttyUSB7', '/dev/ttyUSB8', '/dev/ttyUSB9', '/dev/ttyUSB10',
-                     '/dev/ttyS0', '/dev/ttyS1', '/dev/ttyS2', '/dev/tty.usbserial', '/dev/tty.usbmodem', 'com2',
-                     'com3', 'com4', 'com5', 'com6', 'com7', 'com8', 'com9', 'com10', 'com11', 'com12', 'com13',
-                     'com14', 'com15', 'com16', 'com17', 'com18', 'com19', 'com20', 'com21', 'com1', 'end'
-                     ]
+        # if MAC get list of ports
+        if sys.platform.startswith('darwin'):
+            locations = glob.glob('/dev/tty.[usb*]*')
+            locations.append('end')
+        # for everyone else, here is a list of possible ports
+        else:
+            locations = ['dev/ttyACM0', '/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyACM2', '/dev/ttyACM3', '/dev/ttyACM4',
+                         '/dev/ttyACM5', '/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2', '/dev/ttyUSB3', '/dev/ttyUSB4',
+                         '/dev/ttyUSB5', '/dev/ttyUSB6', '/dev/ttyUSB7', '/dev/ttyUSB8', '/dev/ttyUSB9',
+                         '/dev/ttyUSB10',
+                         '/dev/ttyS0', '/dev/ttyS1', '/dev/ttyS2', '/dev/tty.usbserial', '/dev/tty.usbmodem', 'com2',
+                         'com3', 'com4', 'com5', 'com6', 'com7', 'com8', 'com9', 'com10', 'com11', 'com12', 'com13',
+                         'com14', 'com15', 'com16', 'com17', 'com18', 'com19', 'com20', 'com21', 'com1', 'end'
+                         ]
+
         detected = None
         for device in locations:
             try:
