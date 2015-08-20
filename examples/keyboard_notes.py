42f4941cd2becab017dae142dbee339a705f26a7
David Fisher <fisherds@rose-hulman.edu>
2015-08-20 16:06:28 -0400

    Updated the keyboard example
    pymata 1.7 fixed the need to resend data

--- examples/keyboard_notes.py
+++ examples/keyboard_notes.py
@@ -23,43 +23,41 @@ NOTE_F5 = 698
 
 
 def setup():
-    board.set_pin_mode(BUZZER_PIN, Constants.OUTPUT)  # configures the buzzerPin as an OUTPUT
+    print("Virtual piano")
 
 
 def loop():
     key = input("Enter a key in the range asdfghjkl;' to play a note (Space to stop, x to exit) ")
-    for i in range(2):   # TODO: Avoid sending the command twice once issue #20 is resolved.
-        board.sleep(0.1)
-        if key == "a":
-            board.play_tone(BUZZER_PIN, Constants.TONE_TONE, NOTE_C4, None)
-        elif key == "s":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_D4, None)
-        elif key == "d":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_E4, None)
-        elif key == "f":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_F4, None)
-        elif key == "g":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_G4, None)
-        elif key == "h":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_A4, None)
-        elif key == "j":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_B4, None)
-        elif key == "k":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_C5, None)
-        elif key == "l":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_D5, None)
-        elif key == ";":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_E5, None)
-        elif key == "'":
-            board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_F5, None)
-        elif key == " ":
-            board.play_tone(BUZZER_PIN,Constants.TONE_NO_TONE, 0, 0)
-        elif key == "x":
-            board.shutdown()
-            exit(0)
-        else:
-            print("Unknown key")
-            break
+    if key == "a":
+        board.play_tone(BUZZER_PIN, Constants.TONE_TONE, NOTE_C4, None)
+    elif key == "s":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_D4, None)
+    elif key == "d":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_E4, None)
+    elif key == "f":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_F4, None)
+    elif key == "g":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_G4, None)
+    elif key == "h":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_A4, None)
+    elif key == "j":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_B4, None)
+    elif key == "k":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_C5, None)
+    elif key == "l":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_D5, None)
+    elif key == ";":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_E5, None)
+    elif key == "'":
+        board.play_tone(BUZZER_PIN,Constants.TONE_TONE, NOTE_F5, None)
+    elif key == " ":
+        board.play_tone(BUZZER_PIN,Constants.TONE_NO_TONE, 0, 0)
+    elif key == "x":
+        board.shutdown()
+        exit(0)
+    else:
+        print("Unknown key:", key)
+
 
 if __name__ == "__main__":
     setup()
