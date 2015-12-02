41756d26adef2fce45dc78352cfd47c4ff858860
MisterYsLab@gmail.com <brookwood58>
2015-12-02 12:03:42 -0500

    v2.8

--- pymata_aio/private_constants.py
+++ pymata_aio/private_constants.py
@@ -70,7 +70,7 @@ class PrivateConstants:
     SYSEX_REALTIME = 0x7F  # MIDI Reserved for realtime messages
 
     # reserved for PyMata
-    PYMATA_VERSION = "2.7"
+    PYMATA_VERSION = "2.8"
 
     # each byte represents a digital port
     #  and its value contains the current port settings
